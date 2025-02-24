f969d5af34ccb8be27dfee28723b579ca12a149b
Camden <camden.c.taylor@gmail.com>
2025-02-24 15:04:20 -0700

    configured db connections and created models for task and category

---
+++ Mission08_Team0411/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mission08_Team0411.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public virtual ICollection<Task> Tasks { get; set; } = new List<Task>();
+}
---
+++ Mission08_Team0411/Models/TasksContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mission08_Team0411.Models;
+
+public partial class TasksContext : DbContext
+{
+    public TasksContext()
+    {
+    }
+
+    public TasksContext(DbContextOptions<TasksContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Task> Tasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=tasks.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.ToTable("Category");
+
+            entity.Property(e => e.CategoryId).ValueGeneratedNever();
+        });
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.HasOne(d => d.Category).WithMany(p => p.Tasks).HasForeignKey(d => d.CategoryId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}

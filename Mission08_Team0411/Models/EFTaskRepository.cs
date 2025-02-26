a9ed0e97b5a01b0655abd25503cf0fdee229563c
Grace Farnsworth <gmfarnz@byu.edu>
2025-02-26 15:45:32 -0700

    initial finish of my section

---
+++ Mission08_Team0411/Models/EFTaskRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Mission08_Team0411.Models
+{
+    public class EFTaskRepository : ITaskRepository
+    {
+        private TasksContext _context;
+        public EFTaskRepository(TasksContext temp)
+        {
+            _context = temp;
+        }
+
+        public List<Task> Tasks => _context.Tasks.ToList();
+
+        public void AddTask(Task task)
+        {
+            _context.Add(task);
+            _context.SaveChanges();
+        }
+    }
+}

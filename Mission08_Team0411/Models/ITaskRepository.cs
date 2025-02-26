156bd8ecf85f9de37e0ad084778f374f87646e7f
Grace Farnsworth <gmfarnz@byu.edu>
2025-02-26 16:46:24 -0700

    fixed

---
+++ Mission08_Team0411/Models/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Mission08_Team0411.Models
+{
+    public interface ITaskRepository
+    {
+        List<ToDo> Tasks { get; }
+
+        public void AddTask(ToDo toDo);
+      
+        public void SaveChanges(ToDo toDo);
+
+        public void Update(ToDo toDo);
+    }
+}

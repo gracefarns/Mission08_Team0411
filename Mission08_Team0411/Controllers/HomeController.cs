b49b20801408e7064d882a2811290cf690ebee09
Benson Grotegut <gutblue@Bensons-Air.app.byu.edu>
2025-02-26 15:48:24 -0700

    Index view with Quadrants, added DeleteConfirmation, buttons to Edit and Delete, etc.

--- Mission08_Team0411/Controllers/HomeController.cs
+++ Mission08_Team0411/Controllers/HomeController.cs
@@ -1,32 +1,25 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0411.Models;
 
 namespace Mission08_Team0411.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly TasksContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(TasksContext context)
         {
-            _logger = logger;
+            _context = context;
         }
-
+        
         public IActionResult Index()
         {
-            return View();
+            var tasks = _context.Tasks
+                .Include(t => t.Category).ToList();
+            return View(tasks);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
